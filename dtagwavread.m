b2cbabfdc7c6ecb42219a939c4b154d86bbb2530
Frants Jensen <frants.jensen@gmail.com>
2023-09-20 08:59:50 +0200

    Improved robustness to different prefix format

--- dtagwavread.m
+++ dtagwavread.m
@@ -7,7 +7,14 @@ tagver = dtagtype(tag);
 
 if tagver==3,
     recdir = d3makefname(tag,'RECDIR'); % get audio path
-    [x,afs] = d3wavread([tcue tcue+tdur],recdir, [tag(1:2) tag(6:9)], 'wav' ) ;
+    prefix = [tag(1:2) tag(6:9)];
+    
+    D = dir([recdir prefix '*.wav']);
+    if isempty(D)
+        prefix = tag;
+        D = dir([recdir prefix '*.wav']);
+    end
+    [x,afs] = d3wavread([tcue tcue+tdur],recdir,prefix , 'wav' ) ;
 elseif tagver==2,
     [x,afs] = tagwavread(tag,tcue,tdur);
 end
